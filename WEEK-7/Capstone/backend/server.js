1c91d1260b553d196a507b9758cb87318ddceab7
Madhurimaam12 <24eg105k62@anurag.edu.in>
2026-05-17 22:53:16 +0530

    .

--- WEEK-7/Capstone/backend/server.js
+++ WEEK-7/Capstone/backend/server.js
@@ -26,7 +26,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174", 
       "http://localhost:5175",
-      "https://your-frontend.vercel.app",
+      "https://atp-24-eg-105-k62-eight.vercel.app", 
     ],
     credentials: true,
   })
