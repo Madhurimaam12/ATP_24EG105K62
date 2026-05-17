3a0653e0427204d8df1d337f043ca17493ee80d1
Madhurimaam12 <24eg105k62@anurag.edu.in>
2026-05-17 23:16:46 +0530

    Update Home.jsx

--- WEEK-7/Capstone/frontend/src/components/Home.jsx
+++ WEEK-7/Capstone/frontend/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import api from "../api/axiosClient";
+import commonApi from "../api/axiosClient";
 
 function Home() {
   const [articles, setArticles] = useState([]);
