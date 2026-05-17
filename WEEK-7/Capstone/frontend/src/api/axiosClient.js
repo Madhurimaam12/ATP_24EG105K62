ac4f65852f95117dc7e01d9a7d9e6b8cf89dd4b2
Madhurimaam12 <24eg105k62@anurag.edu.in>
2026-05-17 22:29:08 +0530

    .

--- WEEK-7/Capstone/frontend/src/api/axiosClient.js
+++ WEEK-7/Capstone/frontend/src/api/axiosClient.js
@@ -1,28 +1,31 @@
 import axios from "axios";
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://capstone-24eg105k62.onrender.com";
+
 export const commonApi = axios.create({
-  baseURL: "http://localhost:5000/api/common",
+  baseURL: `${BACKEND_URL}/api/common`,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
 
 export const authorApi = axios.create({
-  baseURL: "http://localhost:5000/author-api",
+  baseURL: `${BACKEND_URL}/author-api`,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
 
 export const adminApi = axios.create({
-  baseURL: "http://localhost:5000/admin-api",
+  baseURL: `${BACKEND_URL}/admin-api`,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
 
 export const userApi = axios.create({
-  baseURL: "http://localhost:5000/user-api",
+  baseURL: `${BACKEND_URL}/user-api`,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
 
+// Default export for backward compatibility
 const api = commonApi;
 export default api;
