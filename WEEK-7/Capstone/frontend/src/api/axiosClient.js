3d3f3bcd2f16218de9fe75b4368c17b062e7b807
Madhurimaam12 <24eg105k62@anurag.edu.in>
2026-05-17 18:45:40 +0530

    Update axiosClient.js

--- WEEK-7/Capstone/frontend/src/api/axiosClient.js
+++ WEEK-7/Capstone/frontend/src/api/axiosClient.js
@@ -1,11 +1,28 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000",
+export const commonApi = axios.create({
+  baseURL: "http://localhost:5000/api/common",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
 });
 
+export const authorApi = axios.create({
+  baseURL: "http://localhost:5000/author-api",
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
+});
+
+export const adminApi = axios.create({
+  baseURL: "http://localhost:5000/admin-api",
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
+});
+
+export const userApi = axios.create({
+  baseURL: "http://localhost:5000/user-api",
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
+});
+
+const api = commonApi;
 export default api;
