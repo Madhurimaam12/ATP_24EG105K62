b7bb8184f04e47077077d9fa02bf3ce5bb5bd935
Madhurimaam12 <24eg105k62@anurag.edu.in>
2026-05-19 14:05:36 +0530

    .

--- WEEK-6/mern-app-emps/frontend/src/components/ListOfEmps.jsx
+++ WEEK-6/mern-app-emps/frontend/src/components/ListOfEmps.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
-import { API_BASE_URL } from "../config";  
+
+const API_BASE_URL = "https://mern-app-emps-backend-8tsc.onrender.com";
 
 function ListOfEmps() {
   const [emps, setEmps] = useState([]);
@@ -16,7 +17,7 @@ function ListOfEmps() {
 
   const deleteEmployee = async (empId) => {
     if (window.confirm("Are you sure you want to delete this employee?")) {
-      let res = await fetch(`${API_BASE_URL}/emp-api/employees/${empId}`, { 
+      let res = await fetch(`${API_BASE_URL}/emp-api/employees/${empId}`, {
         method: "DELETE",
       });
       if (res.status === 200) {
@@ -27,7 +28,7 @@ function ListOfEmps() {
 
   useEffect(() => {
     async function getEmps() {
-      let res = await fetch(`${API_BASE_URL}/emp-api/employees`);  
+      let res = await fetch(`${API_BASE_URL}/emp-api/employees`);
       if (res.status === 200) {
         let resObj = await res.json();
         setEmps(resObj.payload);
@@ -41,12 +42,12 @@ function ListOfEmps() {
       <h1 className="text-4xl text-center">List of Employees</h1>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
         {emps.map((empObj) => (
-          <div key={empObj._id} className=" bg-white p-5 text-center text-2xl rounded-2xl shadow-2xl ">
+          <div key={empObj._id} className="bg-white p-5 text-center text-2xl rounded-2xl shadow-2xl">
             <p>{empObj.email}</p>
             <p className="mb-4">{empObj.name}</p>
             <div className="flex justify-around">
               <button onClick={() => gotoEmpoyee(empObj)} className="bg-green-600 p-2 rounded-2xl text-white">View</button>
-              <button onClick={()=>gotoEditEmpoyee(empObj)} className="bg-yellow-600 p-2 rounded-2xl text-white">Edit</button>
+              <button onClick={() => gotoEditEmpoyee(empObj)} className="bg-yellow-600 p-2 rounded-2xl text-white">Edit</button>
               <button onClick={() => deleteEmployee(empObj._id)} className="bg-red-600 p-2 rounded-2xl text-white">Delete</button>
             </div>
           </div>
