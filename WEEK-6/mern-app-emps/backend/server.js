d89c33846a5ad10cf3d6a76c902e68bdea43807e
Madhurimaam12 <24eg105k62@anurag.edu.in>
2026-05-19 13:41:15 +0530

    .

--- WEEK-6/mern-app-emps/backend/server.js
+++ WEEK-6/mern-app-emps/backend/server.js
@@ -4,23 +4,29 @@ import { empRoute } from "./API/empApp.js";
 import cors from "cors";
 
 const app = exp();
-//add cors middleware
+
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-  }),
+    origin: [
+      "http://localhost:5173", 
+      "http://localhost:5174",
+      "https://mern-app-emps-qq2v17jea-madhurimaam12s-projects.vercel.app",
+      "https://mern-app-emps-k1ideeqbc-madhurimaam12s-projects.vercel.app"
+    ],
+  })
 );
-//body parser middleware
+
 app.use(exp.json());
-//emp api middleware
 app.use("/emp-api", empRoute);
 
-//DB connection
 const connectDB = async () => {
   try {
-    await connect("mongodb+srv://madhurima:madhurima@clusterO.farh4i4.mongodb.net/?appName=ClusterO");
+    const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://madhurima:madhurima@clusterO.farh4i4.mongodb.net/?appName=ClusterO";
+    await connect(MONGODB_URI);
     console.log("DB connected");
-    app.listen(4000, () => console.log("server listening on port 4000.."));
+    
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log(`server listening on port ${PORT}..`));
   } catch (err) {
     console.log("err in DB connection", err.message);
   }
@@ -28,10 +34,8 @@ const connectDB = async () => {
 
 connectDB();
 
-//error handling middleware
 app.use((err, req, res, next) => {
   console.log("err in middleware:", err.message);
-
   res.status(err.status || 500).json({
     message: "error",
     reason: err.message,
